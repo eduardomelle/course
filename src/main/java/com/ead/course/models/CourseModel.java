7ece9d72d3ee3a7338e84a9532bd556c1a19f276
Eduardo <eduardomelle@gmail.com>
2022-06-14 07:28:13 -0300

    Mapear Entidades, criar Repositories e Services para Multiplas Entidades

---
+++ src/main/java/com/ead/course/models/CourseModel.java
@@ -0,0 +1,40 @@
+package com.ead.course.models;
+
+import com.ead.course.enums.CourseLevel;
+import com.ead.course.enums.CourseStatus;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "TB_COURSES")
+public class CourseModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(generator = GenerationType.AUTO)
+    private UUID courseId;
+
+    @Column(nullable = false, length = 255)
+    private String name;
+
+    private String description;
+
+    private String imageUrl;
+
+    private LocalDateTime creationDate;
+
+    private LocalDateTime lastUpdateDate;
+
+    private CourseStatus courseStatus;
+
+    private CourseLevel courseLevel;
+
+    private UUID userInstructor;
+
+}
