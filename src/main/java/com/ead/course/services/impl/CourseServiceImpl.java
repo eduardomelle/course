feffcbc9b3c75482383bd37fec711831173eba9a
Eduardo <eduardomelle@gmail.com>
2022-06-22 06:23:45 -0300

    Deleção de Relacionamento Cascade.ALL ou Método de Deleção Customizado

--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,10 +1,15 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.ModuleModel;
 import com.ead.course.repositories.CourseRepository;
+import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -13,4 +18,13 @@ public class CourseServiceImpl implements CourseService {
     @Autowired
     private CourseRepository courseRepository;
 
+    @Autowired
+    private ModuleRepository moduleRepository;
+
+    @Override
+    public void delete(CourseModel courseModel) {
+
+        List<ModuleModel> moduleModelList = moduleRepository.findAllModulesIntoCourse(courseModel.getCourseId());
+
+    }
 }
