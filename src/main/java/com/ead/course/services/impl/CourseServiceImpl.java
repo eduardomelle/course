fd001e16f8f7ff8f1cb8edf0e86fb9777fab67f0
Eduardo <eduardomelle@gmail.com>
2022-06-26 10:37:39 -0300

    Criar API RESTful para Course com Validação Spring Validation

--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -7,23 +7,27 @@ import com.ead.course.repositories.CourseRepository;
 import com.ead.course.repositories.LessonRepository;
 import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.CourseService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class CourseServiceImpl implements CourseService {
 
-    @Autowired
-    CourseRepository courseRepository;
+    private final CourseRepository courseRepository;
 
-    @Autowired
-    ModuleRepository moduleRepository;
+    private final ModuleRepository moduleRepository;
 
-    @Autowired
-    LessonRepository lessonRepository;
+    private final LessonRepository lessonRepository;
+
+    public CourseServiceImpl(CourseRepository courseRepository, ModuleRepository moduleRepository, LessonRepository lessonRepository) {
+        this.courseRepository = courseRepository;
+        this.moduleRepository = moduleRepository;
+        this.lessonRepository = lessonRepository;
+    }
 
     @Transactional
     @Override
@@ -44,6 +48,11 @@ public class CourseServiceImpl implements CourseService {
     @Override
     public CourseModel save(CourseModel courseModel) {
         return courseRepository.save(courseModel);
+    }
+
+    @Override
+    public Optional<CourseModel> findById(UUID courseId) {
+        return courseRepository.findById(courseId);
     }
 
 }
