a29f5570fb78ffb104dc5186d39880a4c74dd2c5
Eduardo <eduardomelle@gmail.com>
2022-07-10 10:41:02 -0300

    Filtros Avançados em APIs com Specification e Pageable

--- src/main/java/com/ead/course/controllers/CourseController.java
+++ src/main/java/com/ead/course/controllers/CourseController.java
@@ -3,7 +3,11 @@ package com.ead.course.controllers;
 import com.ead.course.dtos.CourseDto;
 import com.ead.course.models.CourseModel;
 import com.ead.course.services.CourseService;
+import com.ead.course.specifications.SpecificationTemplate;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -63,7 +67,9 @@ public class CourseController {
     }
 
     @GetMapping
-    public ResponseEntity<List<CourseModel>> getAllCourses() {
+    public ResponseEntity<List<CourseModel>> getAllCourses(
+            SpecificationTemplate.CourseSpec spec,
+            @PageableDefault(page = 0, size = 10, sort = "courseId", direction = Sort.Direction.ASC) Pageable pageable) {
         return ResponseEntity.ok(courseService.findAll());
     }
 
