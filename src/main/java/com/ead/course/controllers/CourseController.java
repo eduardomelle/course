e4193e4e7101f114eb7c8df00d88b28b7dc10b0a
Eduardo <eduardomelle@gmail.com>
2022-08-05 07:58:28 -0300

    API Composition Pattern - Preparar Métodos GetAll

--- src/main/java/com/ead/course/controllers/CourseController.java
+++ src/main/java/com/ead/course/controllers/CourseController.java
@@ -71,6 +71,10 @@ public class CourseController {
             SpecificationTemplate.CourseSpec spec,
             @PageableDefault(page = 0, size = 10, sort = "courseId", direction = Sort.Direction.ASC) Pageable pageable,
             @RequestParam(required = false) UUID userId) {
+        if (userId != null) {
+            return ResponseEntity.ok(courseService.findAll(SpecificationTemplate.courseUserId(userId).and(spec), pageable));
+        }
+
         return ResponseEntity.ok(courseService.findAll(spec, pageable));
     }
 
